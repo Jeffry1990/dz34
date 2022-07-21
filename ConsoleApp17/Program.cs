eae282e700927ad90406f2f0a3531d416bac5315
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-21 14:42:32 +0300

    Add files via upload

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomMaxNumber = 101;
+            int specifiedNumber1 = 3;
+            int specifiedNumber2 = 5;
+            Random random = new Random();
+            int number = random.Next(randomMaxNumber);
+
+            Console.WriteLine("Случайное число: "+number);
+            Console.ReadLine();
+
+            for (int i = specifiedNumber1; i <= number; i ++)
+            {
+                if (i % specifiedNumber1 == 0 || i % specifiedNumber2 == 0)
+                {
+                    Console.WriteLine(i);
+                    Console.ReadLine();
+                }
+                }                               
+            }
+        }
+    }
